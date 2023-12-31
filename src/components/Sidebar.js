f3d547d6cb9e0dffebe1f66d64ab2a200b12470a
Khushboo Singh <singhkhushboo498.1@gmail.com>
2023-12-31 13:09:36 +0530

    code update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,9 @@
 import { NavLink } from "react-router-dom";
-import { useState } from 'react';
 
 
 const Sidebar = ({isSidebarOpen}) =>{
-    const [isClose, setClose] = useState(isSidebarOpen);
-    // const handleClose = ()=>{
-    //     setClose((prev)=>!prev);
-    // }
     return (
         <div className={isSidebarOpen ? 'sidebar active' : 'sidebar'}>
-            {/* <button onClick={handleClose}><i className="fa-solid fa-xmark"></i></button> */}
             <div className="sidebar__top">
             <NavLink to='/' className='nav-item'>
             <i class="fa-brands fa-windows"></i> Dashboard</NavLink>
